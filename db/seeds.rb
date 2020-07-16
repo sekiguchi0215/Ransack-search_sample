be74842d9bf57da4dd9d8ee0057ee32da04f6cf1
vagrant <vagrant@localhost.localdomain>
2020-07-16 13:37:16 +0000

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'けんたろう',age: '24', sex: '1', address: '新宿')
+User.create(name: 'けんじ',age: '25', sex: '1', address: '渋谷')
+User.create(name: 'かな',age: '21', sex: '2', address: '原宿')
+User.create(name: 'みき',age: '17', sex: '2', address: '新宿')
+User.create(name: 'かんな',age: '22', sex: '2', address: '池袋')
+User.create(name: 'あきお',age: '13', sex: '1', address: '上野')
